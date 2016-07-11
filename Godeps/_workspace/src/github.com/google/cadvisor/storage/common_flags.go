fd7cc732414a82e43bededd4e586f5ff5e7f57ae
uriwang <uriwang@tencent.com>
2016-07-11 09:14:56 +0800

    uri:delete uri struct defination

--- Godeps/_workspace/src/github.com/google/cadvisor/storage/common_flags.go
+++ Godeps/_workspace/src/github.com/google/cadvisor/storage/common_flags.go
@@ -15,12 +15,7 @@
 package storage
 
 import (
-	"bytes"
 	"flag"
-	"fmt"
-	"net/url"
-	"os"
-	"strings"
 	"time"
 )
 
@@ -34,57 +29,3 @@ var ArgKubeletIp = flag.String("storage_kubelet_address", "127.0.0.1", "kubelet 
 var ArgKubeletPort = flag.Int("storage_kubelet_port", 10255, "kubelet api port")
 var ArgDataId = flag.Uint64("storage_dataid", 0, "report data to the storage by this dataid, default is -1")
 var ArgDbBufferDuration = flag.Duration("storage_driver_buffer_duration", 60*time.Second, "Writes in the storage driver will be buffered for this duration, and committed to the non memory backends as a single transaction")
-
-type Uri struct {
-	Key string
-	Val url.URL
-}
-
-func (u *Uri) String() string {
-	val := u.Val.String()
-	if val == "" {
-		return fmt.Sprintf("%s", u.Key)
-	}
-	return fmt.Sprintf("%s:%s", u.Key, val)
-}
-
-func (u *Uri) Set(value string) error {
-	s := strings.SplitN(value, ":", 2)
-	if s[0] == "" {
-		return fmt.Errorf("missing uri key in '%s'", value)
-	}
-	u.Key = s[0]
-	if len(s) > 1 && s[1] != "" {
-		e := os.ExpandEnv(s[1])
-		uri, err := url.Parse(e)
-		if err != nil {
-			return err
-		}
-		u.Val = *uri
-	}
-	return nil
-}
-
-type Uris []Uri
-
-func (us *Uris) String() string {
-	var b bytes.Buffer
-	b.WriteString("[")
-	for i, u := range *us {
-		if i > 0 {
-			b.WriteString(" ")
-		}
-		b.WriteString(u.String())
-	}
-	b.WriteString("]")
-	return b.String()
-}
-
-func (us *Uris) Set(value string) error {
-	var u Uri
-	if err := u.Set(value); err != nil {
-		return err
-	}
-	*us = append(*us, u)
-	return nil
-}
